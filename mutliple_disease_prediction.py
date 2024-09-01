a1ca25de3f6022ea3c466c43efcb32d9bf2f3385
sujal chondhekar <157092195+sujalappa@users.noreply.github.com>
2024-09-01 14:51:37 +0530

    Update mutliple_disease_prediction.py

--- mutliple_disease_prediction.py
+++ mutliple_disease_prediction.py
@@ -11,9 +11,9 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 #loading the models
-diabetes_model = pickle.load(open('C:/Users/sujal/OneDrive/Desktop/multiple disease prediction/diabetes_model.sav','rb'))
-heart_disease_model = pickle.load(open('C:/Users/sujal/OneDrive/Desktop/multiple disease prediction/heart_disease_model.sav','rb'))
-parkinsons_model = pickle.load(open('C:/Users/sujal/OneDrive/Desktop/multiple disease prediction/parkinsons_model.sav','rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav','rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav','rb'))
 
 
 # sidebar for navigate
